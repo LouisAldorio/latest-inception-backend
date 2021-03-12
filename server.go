4bc763cdeef681c9fe1ef12af2bd49f8c661dbbf
felixyangsen <felix@pmberjaya.com>
2021-03-12 13:43:29 +0700

    env file

--- server.go
+++ server.go
@@ -13,12 +13,15 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
 	"github.com/rs/cors"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	godotenv.Load(".env")
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
@@ -45,5 +48,5 @@ func main() {
 }
 
 func main1() {
-	config.MigrateSql()	
+	config.MigrateSql()
 }
