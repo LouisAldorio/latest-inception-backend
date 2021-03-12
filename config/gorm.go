f2fbeed6dc7dd2ff18bd06db8722d752b5596fc9
felixyangsen <felix@pmberjaya.com>
2021-03-12 13:50:57 +0700

    minor bug

--- config/gorm.go
+++ config/gorm.go
@@ -12,10 +12,10 @@ import (
 	"gorm.io/gorm/schema"
 )
 
-var DSN = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require TimeZone=Asia/Jakarta", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_DATABASE"), os.Getenv("DB_PORT"))
-
 //ConnectDB connect GO to database
 func ConnectDB() (*gorm.DB, *sql.DB) {
+	var DSN = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require TimeZone=Asia/Jakarta", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_DATABASE"), os.Getenv("DB_PORT"))
+
 	var postgres = postgres.New(postgres.Config{
 		DSN:                  DSN,
 		PreferSimpleProtocol: true,
