dd690e3584d99e06646e7492015ec0cc814e8180
louisaldorio <louisaldorio@gmail.com>
2021-06-03 11:15:14 +0700

    print env variable

--- config/gorm.go
+++ config/gorm.go
@@ -16,6 +16,8 @@ import (
 func ConnectDB() (*gorm.DB, *sql.DB) {
 	var dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE"))
 
+	fmt.Println(dsn, os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE"))
+
 	var mysql = mysql.New(mysql.Config{
 		DSN: dsn,
 	})
