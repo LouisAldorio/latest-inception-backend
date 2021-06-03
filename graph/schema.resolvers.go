fa3d739ffee0c8072efaef78f2432e6bb351221b
louisaldorio <louisaldorio@gmail.com>
2021-06-03 12:27:11 +0700

    Finishing UP gitlab CI

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -65,7 +65,7 @@ func (r *queryResolver) CategoryList(ctx context.Context) ([]*model.Category, er
 }
 
 func (r *queryResolver) HelloWorld(ctx context.Context) (string, error) {
-	return "Hello World this shows deployment is success though ssh!",nil
+	return "Last Attempt Succes!",nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
