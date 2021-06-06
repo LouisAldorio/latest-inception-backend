ca5abde081b40baee688b90394baf4b5cdbe36c0
louisaldorio <louisaldorio@gmail.com>
2021-06-06 14:49:56 +0700

    try CI/CD

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -65,7 +65,7 @@ func (r *queryResolver) CategoryList(ctx context.Context) ([]*model.Category, er
 }
 
 func (r *queryResolver) HelloWorld(ctx context.Context) (string, error) {
-	return "Last Attempt Succes prune!",nil
+	return "Last Attempt Succes prune!, more",nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
